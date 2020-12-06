e0bec1f04ae350fa997ff1b16f1eda8c504504bf
vologiroff <volkant0778@ya.ru>
2020-12-06 16:05:19 +0300

    Question class added
    Answer and questions refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,13 +27,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
 
-  List<bool> answers = [false, true, true];
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
 
   int questionNumber = 0;
 
@@ -48,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +75,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer =
+                      questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == true) {
                     print('user got it right!');
                     scoreKeeper.add(Icon(
@@ -109,7 +112,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer =
+                      questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == false) {
                     print('user got it right!');
                     scoreKeeper.add(Icon(
